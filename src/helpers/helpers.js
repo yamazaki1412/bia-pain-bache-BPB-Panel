6178c0bd8987df05095cc9473a379ea61e1c75f7
yamazaki1412 <62888588+yamazaki1412@users.noreply.github.com>
2025-04-29 16:55:34 +0800

    Add files via upload

---
+++ src/helpers/helpers.js
@@ -0,0 +1,141 @@
+import { Authenticate } from "../authentication/auth";
+import { extractWireguardParams } from "../cores-configs/helpers";
+import { getDataset, updateDataset } from "../kv/handlers";
+import { renderHomePage } from "../pages/home";
+import JSZip from "jszip";
+
+export function isValidUUID(uuid) {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(uuid);
+}
+
+export async function resolveDNS(domain) {
+    const dohURL = 'https://cloudflare-dns.com/dns-query';
+    const dohURLv4 = `${dohURL}?name=${encodeURIComponent(domain)}&type=A`;
+    const dohURLv6 = `${dohURL}?name=${encodeURIComponent(domain)}&type=AAAA`;
+
+    try {
+        const [ipv4Response, ipv6Response] = await Promise.all([
+            fetch(dohURLv4, { headers: { accept: 'application/dns-json' } }),
+            fetch(dohURLv6, { headers: { accept: 'application/dns-json' } })
+        ]);
+
+        const ipv4Addresses = await ipv4Response.json();
+        const ipv6Addresses = await ipv6Response.json();
+
+        const ipv4 = ipv4Addresses.Answer
+            ? ipv4Addresses.Answer.map((record) => record.data)
+            : [];
+        const ipv6 = ipv6Addresses.Answer
+            ? ipv6Addresses.Answer.map((record) => record.data)
+            : [];
+
+        return { ipv4, ipv6 };
+    } catch (error) {
+        console.error('Error resolving DNS:', error);
+        throw new Error(`An error occurred while resolving DNS - ${error}`);
+    }
+}
+
+export function isDomain(address) {
+    const domainPattern = /^(?!\-)(?:[A-Za-z0-9\-]{1,63}\.)+[A-Za-z]{2,}$/;
+    return domainPattern.test(address);
+}
+
+export async function handlePanel(request, env) {
+    const auth = await Authenticate(request, env);
+    if (request.method === 'POST') {
+        if (!auth) return new Response('Unauthorized or expired session!', { status: 401 });
+        await updateDataset(request, env);
+        return new Response('Success', { status: 200 });
+    }
+
+    const { proxySettings } = await getDataset(request, env);
+    const pwd = await env.kv.get('pwd');
+    if (pwd && !auth) return Response.redirect(`${globalThis.urlOrigin}/login`, 302);
+    const isPassSet = pwd?.length >= 8;
+    return await renderHomePage(proxySettings, isPassSet);
+}
+
+export async function fallback(request) {
+    const url = new URL(request.url);
+    url.hostname = globalThis.fallbackDomain;
+    url.protocol = 'https:';
+    const newRequest = new Request(url.toString(), {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+        redirect: 'manual'
+    });
+
+    return await fetch(newRequest);
+}
+
+export async function getMyIP(request) {
+    const ip = await request.text();
+    try {
+        const response = await fetch(`http://ip-api.com/json/${ip}?nocache=${Date.now()}`);
+        const geoLocation = await response.json();
+        return new Response(JSON.stringify(geoLocation), {
+            status: 200,
+            headers: {
+                'Content-Type': 'text/plain;charset=utf-8'
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+    }
+}
+
+export async function getWarpConfigs(request, env, isPro) {
+    const auth = await Authenticate(request, env);
+    if (!auth) return new Response('Unauthorized or expired session!', { status: 401 });
+    const { warpConfigs, proxySettings } = await getDataset(request, env);
+    const { warpEndpoints, amneziaNoiseCount, amneziaNoiseSizeMin, amneziaNoiseSizeMax } = proxySettings;
+    const warpConfig = extractWireguardParams(warpConfigs, false);
+    const { warpIPv6, publicKey, privateKey } = warpConfig;
+    const zip = new JSZip();
+    const trimLines = (string) => string.split("\n").map(line => line.trim()).join("\n");
+    const amneziaNoise = isPro ? trimLines(
+        `Jc = ${amneziaNoiseCount}
+        Jmin = ${amneziaNoiseSizeMin}
+        Jmax = ${amneziaNoiseSizeMax}
+        S1 = 0
+        S2 = 0
+        H1 = 0
+        H2 = 0
+        H3 = 0
+        H4 = 0`
+    ) : '';
+
+    try {
+        warpEndpoints.split(',').forEach((endpoint, index) => {
+            zip.file(`BPB-Warp-${index + 1}.conf`, trimLines(
+                `[Interface]
+                PrivateKey = ${privateKey}
+                Address = 172.16.0.2/32, ${warpIPv6}
+                DNS = 1.1.1.1, 1.0.0.1
+                MTU = 1280
+                ${amneziaNoise}
+                [Peer]
+                PublicKey = ${publicKey}
+                AllowedIPs = 0.0.0.0/0, ::/0
+                Endpoint = ${endpoint}
+                PersistentKeepalive = 25`
+            ));
+        });
+    
+        const zipBlob = await zip.generateAsync({ type: "blob" });
+        const arrayBuffer = await zipBlob.arrayBuffer();
+        return new Response(arrayBuffer, {
+            headers: {
+                "Content-Type": "application/zip",
+                "Content-Disposition": `attachment; filename="BPB-Warp-${isPro ? "Pro-" : ""}configs.zip"`,
+            },
+        });
+    } catch (error) {
+        return new Response("Error generating ZIP file.", { status: 500 });
+    }
+}
+
+
---
+++ src/kv/handlers.js
@@ -0,0 +1,173 @@
+import { fetchWarpConfigs } from '../protocols/warp';
+import { Authenticate } from '../authentication/auth';
+
+export async function getDataset(request, env) {
+    let proxySettings, warpConfigs;
+
+    try {
+        proxySettings = await env.kv.get("proxySettings", {type: 'json'});
+        warpConfigs = await env.kv.get('warpConfigs', {type: 'json'});
+    } catch (error) {
+        console.log(error);
+        throw new Error(`An error occurred while getting KV - ${error}`);
+    }
+
+    if (!proxySettings) {
+        proxySettings = await updateDataset(request, env);
+        const { error, configs } = await fetchWarpConfigs(env, proxySettings);
+        if (error) throw new Error(`An error occurred while getting Warp configs - ${error}`);
+        warpConfigs = configs;
+    }
+    
+    if (globalThis.panelVersion !== proxySettings.panelVersion) proxySettings = await updateDataset(request, env);
+    return { proxySettings, warpConfigs }
+}
+
+export async function updateDataset (request, env) {
+    let newSettings = request.method === 'POST' ? await request.formData() : null;
+    const isReset = newSettings?.get('resetSettings') === 'true';
+    let currentSettings;
+    let udpNoises = [];
+    if (!isReset) {
+        try {
+            currentSettings = await env.kv.get("proxySettings", {type: 'json'});
+        } catch (error) {
+            console.log(error);
+            throw new Error(`An error occurred while getting current KV settings - ${error}`);
+        }
+        const udpNoiseModes = newSettings?.getAll('udpXrayNoiseMode') || [];
+        const udpNoisePackets = newSettings?.getAll('udpXrayNoisePacket') || [];
+        const udpNoiseDelaysMin = newSettings?.getAll('udpXrayNoiseDelayMin') || [];
+        const udpNoiseDelaysMax = newSettings?.getAll('udpXrayNoiseDelayMax') || [];
+        const udpNoiseCount = newSettings?.getAll('udpXrayNoiseCount') || [];
+        udpNoises.push(...udpNoiseModes.map((mode, index) => ({
+            type: mode,
+            packet: udpNoisePackets[index],
+            delay: `${udpNoiseDelaysMin[index]}-${udpNoiseDelaysMax[index]}`,
+            count: udpNoiseCount[index]
+        })));
+    } else {
+        newSettings = null;
+    }
+
+    const validateField = (field) => {
+        const fieldValue = newSettings?.get(field);
+        if (fieldValue === undefined) return null;
+        if (fieldValue === 'true') return true;
+        if (fieldValue === 'false') return false;
+        return fieldValue;
+    }
+
+    const proxySettings = {
+        remoteDNS: validateField('remoteDNS') ?? currentSettings?.remoteDNS ?? 'https://8.8.8.8/dns-query',
+        localDNS: validateField('localDNS') ?? currentSettings?.localDNS ?? '8.8.8.8',
+        VLTRFakeDNS: validateField('VLTRFakeDNS') ?? currentSettings?.VLTRFakeDNS ?? false,
+        proxyIP: validateField('proxyIP')?.replaceAll(' ', '') ?? currentSettings?.proxyIP ?? '',
+        outProxy: validateField('outProxy') ?? currentSettings?.outProxy ?? '',
+        outProxyParams: extractChainProxyParams(validateField('outProxy')) ?? currentSettings?.outProxyParams ?? {},
+        cleanIPs: validateField('cleanIPs')?.replaceAll(' ', '') ?? currentSettings?.cleanIPs ?? '',
+        enableIPv6: validateField('enableIPv6') ?? currentSettings?.enableIPv6 ?? true,
+        customCdnAddrs: validateField('customCdnAddrs')?.replaceAll(' ', '') ?? currentSettings?.customCdnAddrs ?? '',
+        customCdnHost: validateField('customCdnHost')?.trim() ?? currentSettings?.customCdnHost ?? '',
+        customCdnSni: validateField('customCdnSni')?.trim() ?? currentSettings?.customCdnSni ?? '',
+        bestVLTRInterval: validateField('bestVLTRInterval') ?? currentSettings?.bestVLTRInterval ?? '30',
+        VLConfigs: validateField('VLConfigs') ?? currentSettings?.VLConfigs ?? true,
+        TRConfigs: validateField('TRConfigs') ?? currentSettings?.TRConfigs ?? false,
+        ports: validateField('ports')?.split(',') ?? currentSettings?.ports ?? ['443'],
+        lengthMin: validateField('fragmentLengthMin') ?? currentSettings?.lengthMin ?? '100',
+        lengthMax: validateField('fragmentLengthMax') ?? currentSettings?.lengthMax ?? '200',
+        intervalMin: validateField('fragmentIntervalMin') ?? currentSettings?.intervalMin ?? '1',
+        intervalMax: validateField('fragmentIntervalMax') ?? currentSettings?.intervalMax ?? '1',
+        fragmentPackets: validateField('fragmentPackets') ?? currentSettings?.fragmentPackets ?? 'tlshello',
+        bypassLAN: validateField('bypass-lan') ?? currentSettings?.bypassLAN ?? false,
+        bypassIran: validateField('bypass-iran') ?? currentSettings?.bypassIran ?? false,
+        bypassChina: validateField('bypass-china') ?? currentSettings?.bypassChina ?? false,
+        bypassRussia: validateField('bypass-russia') ?? currentSettings?.bypassRussia ?? false,
+        blockAds: validateField('block-ads') ?? currentSettings?.blockAds ?? false,
+        blockPorn: validateField('block-porn') ?? currentSettings?.blockPorn ?? false,
+        blockUDP443: validateField('block-udp-443') ?? currentSettings?.blockUDP443 ?? false,
+        customBypassRules: validateField('customBypassRules')?.replaceAll(' ', '') ?? currentSettings?.customBypassRules ?? '',
+        customBlockRules: validateField('customBlockRules')?.replaceAll(' ', '') ?? currentSettings?.customBlockRules ?? '',
+        warpEndpoints: validateField('warpEndpoints')?.replaceAll(' ', '') ?? currentSettings?.warpEndpoints ?? 'engage.cloudflareclient.com:2408',
+        warpFakeDNS: validateField('warpFakeDNS') ?? currentSettings?.warpFakeDNS ?? false,
+        warpEnableIPv6: validateField('warpEnableIPv6') ?? currentSettings?.warpEnableIPv6 ?? true,
+        bestWarpInterval: validateField('bestWarpInterval') ?? currentSettings?.bestWarpInterval ?? '30',
+        xrayUdpNoises: (udpNoises.length ? JSON.stringify(udpNoises) : currentSettings?.xrayUdpNoises) ?? JSON.stringify([
+            {
+                type: 'base64',
+                packet: btoa(globalThis.userID),
+                delay: '1-1',
+                count: '1'
+            }
+        ]),
+        hiddifyNoiseMode: validateField('hiddifyNoiseMode') ?? currentSettings?.hiddifyNoiseMode ?? 'm4',
+        nikaNGNoiseMode: validateField('nikaNGNoiseMode') ?? currentSettings?.nikaNGNoiseMode ?? 'quic',
+        noiseCountMin: validateField('noiseCountMin') ?? currentSettings?.noiseCountMin ?? '10',
+        noiseCountMax: validateField('noiseCountMax') ?? currentSettings?.noiseCountMax ?? '15',
+        noiseSizeMin: validateField('noiseSizeMin') ?? currentSettings?.noiseSizeMin ?? '5',
+        noiseSizeMax: validateField('noiseSizeMax') ?? currentSettings?.noiseSizeMax ?? '10',
+        noiseDelayMin: validateField('noiseDelayMin') ?? currentSettings?.noiseDelayMin ?? '1',
+        noiseDelayMax: validateField('noiseDelayMax') ?? currentSettings?.noiseDelayMax ?? '1',
+        amneziaNoiseCount: validateField('amneziaNoiseCount') ?? currentSettings?.amneziaNoiseCount ?? '5',
+        amneziaNoiseSizeMin: validateField('amneziaNoiseSizeMin') ?? currentSettings?.amneziaNoiseSizeMin ?? '50',
+        amneziaNoiseSizeMax: validateField('amneziaNoiseSizeMax') ?? currentSettings?.amneziaNoiseSizeMax ?? '100',
+        panelVersion: globalThis.panelVersion
+    };
+
+    try {    
+        await env.kv.put("proxySettings", JSON.stringify(proxySettings));
+        if (isReset) await updateWarpConfigs(request, env);          
+    } catch (error) {
+        console.log(error);
+        throw new Error(`An error occurred while updating KV - ${error}`);
+    }
+
+    return proxySettings;
+}
+
+function extractChainProxyParams(chainProxy) {
+    let configParams = {};
+    if (!chainProxy) return {};
+    const url = new URL(chainProxy);
+    const protocol = url.protocol.slice(0, -1);
+    if (protocol === atob('dmxlc3M=')) {
+        const params = new URLSearchParams(url.search);
+        configParams = {
+            protocol: protocol,
+            uuid : url.username,
+            server : url.hostname,
+            port : url.port
+        };
+    
+        params.forEach( (value, key) => {
+            configParams[key] = value;
+        });
+    } else {
+        configParams = {
+            protocol: protocol, 
+            user : url.username,
+            pass : url.password,
+            server : url.host,
+            port : url.port
+        };
+    }
+
+    return JSON.stringify(configParams);
+}
+
+export async function updateWarpConfigs(request, env) {
+    const auth = await Authenticate(request, env); 
+    if (!auth) return new Response('Unauthorized', { status: 401 });
+    if (request.method === 'POST') {
+        try {
+            const { error: warpPlusError } = await fetchWarpConfigs(env);
+            if (warpPlusError) return new Response(warpPlusError, { status: 400 });
+            return new Response('Warp configs updated successfully', { status: 200 });
+        } catch (error) {
+            console.log(error);
+            return new Response(`An error occurred while updating Warp configs! - ${error}`, { status: 500 });
+        }
+    } else {
+        return new Response('Unsupported request', { status: 405 });
+    }
+}
